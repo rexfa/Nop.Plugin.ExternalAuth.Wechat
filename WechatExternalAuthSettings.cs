bb54c7ab84f9ea2c8134d93fa3c09aba5ff548fb
rexfa <rex_fa@163.com>
2022-04-13 00:03:09 +0800

    开始

---
+++ WechatExternalAuthSettings.cs
@@ -0,0 +1,20 @@
+﻿using Nop.Core.Configuration;
+
+
+namespace Nop.Plugin.ExternalAuth.Wechat
+{
+    /// <summary>
+    /// 微信的外部授权设置
+    /// </summary>
+    public class WechatExternalAuthSettings : ISettings
+    {
+        /// <summary>
+        /// 微信端获取的APPID
+        /// </summary>
+        public string AppID { get; set; }
+        /// <summary>
+        /// 密码
+        /// </summary>
+        public string AppSecret { get; set; }
+    }
+}
